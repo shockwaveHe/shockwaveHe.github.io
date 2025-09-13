12f73d78d7b6b3bc78b26361b5a200d59e839dd5
shockwaveHe <57560216+shockwaveHe@users.noreply.github.com>
2025-09-13 05:48:57 +0000

    Deploying to gh-pages from @ shockwaveHe/shockwaveHe.github.io@e7b2f92f8129f7d040319450581c0d976a7b0548 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -384,6 +384,16 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
+            },},{id: "courseprojects-airsim-project",
+          title: 'AirSim Project',
+          description: "UAV-Vehicle Interaction Simulation Platform for course ERG2810 at CUHK-Shenzhen",
+          section: "Courseprojects",handler: () => {
+              window.location.href = "/courseprojects/AirSim/";
+            },},{id: "courseprojects-service-robot",
+          title: 'Service Robot',
+          description: "Service Robot of cource ECE4310 at CUHK-Shenzhen",
+          section: "Courseprojects",handler: () => {
+              window.location.href = "/courseprojects/ServiceRobot/";
             },},{id: "news-a-simple-inline-announcement",
           title: 'A simple inline announcement.',
           description: "",
@@ -395,51 +405,26 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-multispectral-inertial-odometry",
+          title: 'MultiSpectral Inertial Odometry',
+          description: "",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+              window.location.href = "/projects/MSO/";
+            },},{id: "projects-shared-airspace",
+          title: 'Shared Airspace',
+          description: "Safe and Seamless Operation of Crewed and Uncrewed Aircraft in Shared Airspace",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+              window.location.href = "/projects/Shared_Airspace/";
+            },},{id: "projects-toddlerbot-2-0",
+          title: 'Toddlerbot 2.0',
+          description: "Advanced Humanoid Robot Learning Platform",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+              window.location.href = "/projects/Toddlerbot_2.0/";
+            },},{id: "projects-wildfire-uas",
+          title: 'Wildfire UAS',
+          description: "Wildland Fire Safety Monitoring",
           section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+              window.location.href = "/projects/Wildfire_UAS/";
             },},{
         id: 'social-email',
         title: 'email',
